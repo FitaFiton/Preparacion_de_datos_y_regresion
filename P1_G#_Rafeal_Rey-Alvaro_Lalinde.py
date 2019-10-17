2c383c89cac808f826f0595b6977144d8f1ea151
FitaFiton <44345127+FitaFiton@users.noreply.github.com>
2019-10-17 13:15:27 +0200

    update

--- P1_G#_Rafeal_Rey-Alvaro_Lalinde.py
+++ P1_G#_Rafeal_Rey-Alvaro_Lalinde.py
@@ -4,9 +4,9 @@ from sklearn import datasets
 import numpy as np
 import functools
 
-# Metodo sacartuplas: 
-# Funcion: elimina las columnas de X_train y X_test que no aporten demasiada informacion(varianza<0.5)
-#          tambien elimina datos de la columna eliminada de varianza, media, mediana, IQR y desviacion típica
+# Metodo sacartuplas:
+# Funcion: elimina las variables de X_train y X_test que no aporten demasiada informacion(varianza<0.5)
+#          tambien elimina datos de la variable eliminada de varianza, media, mediana, IQR y desviacion típica
 # Parametros que recibe: X_train, X_test, varianza, media, mediana, IQR y desviacion_tipica
 # Devuelve: X_train, X_test, varianza, media, mediana, IQR y desviacion_tipica modificados
 def sacartuplas(X_train, X_test, varianza, media, mediana, IQR, desviacion_tipica):
@@ -22,7 +22,7 @@ def sacartuplas(X_train, X_test, varianza, media, mediana, IQR, desviacion_tipic
             print("Eliminamos variable(feature selection)")
     return X_train, X_test, varianza, media, mediana, IQR, desviacion_tipica
 
-# Metodo sacartuplas: 
+# Metodo sacartuplas:
 # Funcion: elimina las columnas de X_train y X_test que no aporten demasiada informacion(varianza<0.5)
 #          tambien elimina datos de la columna eliminada de varianza, media, mediana, IQR y desviacion típica
 # Parametros que recibe: X_train, X_test, varianza, media, mediana, IQR y desviacion_tipica
@@ -40,9 +40,9 @@ def sacartuplas(X_train, X_test, varianza, media, mediana, IQR, desviacion_tipic
     return X_train, X_test, varianza, media, mediana, IQR, desviacion_tipica
 
 
-# Metodo normalizacionX: 
+# Metodo normalizacionX:
 # Funcion: Normaliza todos los datos del dataset que recibe
-# Parametros que recibe: Dataset, array con los valores de la mediana, array con los valores del IQR, 
+# Parametros que recibe: Dataset, array con los valores de la mediana, array con los valores del IQR,
 #                        array con los valores de la media y array con los valores de la desviacion tipica.
 # Devuelve: Dataset normalizado
 def normalizacionX(datos,mediana,IQR,media, desviaciontipica):
@@ -54,24 +54,24 @@ def normalizacionX(datos,mediana,IQR,media, desviaciontipica):
 
     return(datos)
 
-# Metodo calcularh: 
+# Metodo calcularh:
 # Funcion: Calcula las prediciones en funcion de las zetas recibidas
 # Parametros que recibe: Lista de datos, array con los valores de las thetas y theta0
 # Devuelve: predicion
 def calcularh(X_train,th,th0):
-    
+
     h=th0+X_train@th
-    
+
     return h
 
-# Metodo calcularzetas: 
-# Funcion: Calcula las nuevas thetas y theta0 para minimizar la funcion de error 
+# Metodo calcularzetas:
+# Funcion: Calcula las nuevas thetas y theta0 para minimizar la funcion de error
 # Parametros que recibe: Dataset de datos de entrada, dataser de datos de salida, alpha,
 #                        lista de thetas y theta0
 # Devuelve: nuevas thetas y theta0
 def calcularthetas(X_train,y_train,alpha,th0,th,h):
     #Calaculo de zeta0
-    
+
     w=((alpha/(len(X_train)))*(np.sum(h-y_train)))
     th0=th0-w
 
@@ -79,33 +79,33 @@ def calcularthetas(X_train,y_train,alpha,th0,th,h):
     E=(alpha/(len(X_train)))*(((h-y_train)@X_train))
     #Calculo de la nuevas zetas
     th=th-E
-        
+
     return th0, th
-# Metodo eliminarRuido: 
+# Metodo eliminarRuido:
 # Funcion: Elimina las tuplas en las que tengamos ruido  para una mejor aproximacion
-# Parametros que recibe: Dataset de entradas, array de salidas, array con los valores 
+# Parametros que recibe: Dataset de entradas, array de salidas, array con los valores
 #  de la desviacion tipica, array con los valores de la media
 # Devuelve: Dataset modificado
 def eliminarRuido(datosX, datosY, IQR, mediana):
-    
+
     ruidoeliminado=0
     for tuplasI, tuplas in reversed(list(enumerate(datosX))):
         valores_ruidosos=0
         for iteracionI, iteracion in enumerate(tuplas):
             max = mediana[iteracionI]+ IQR[iteracionI] *1.5
             min = mediana[iteracionI] - IQR[iteracionI] *1.5
-            
+
             if iteracion>max or iteracion<min:
                 valores_ruidosos=valores_ruidosos+1
-        
+
 
         if valores_ruidosos >=1 :
             datosX=np.delete(datosX, tuplasI, axis=0)
             datosY=np.delete(datosY, tuplasI)
             ruidoeliminado=ruidoeliminado+1
     print(ruidoeliminado,"Tuplas de ruido eliminadas")
-            
-            
+
+
     return datosX, datosY
 
 class Preprocessor(object):
@@ -117,10 +117,10 @@ class Preprocessor(object):
         self.desviacion_tipica = 0
         self.media = 0
         self.IQR = 0
-    
+
     def fit(self, X, y=None):
         '''
-        Recibe los datos de entrada X y opcionalmente los datos de salida esperados y para preparar los modelos necesarios para 
+        Recibe los datos de entrada X y opcionalmente los datos de salida esperados y para preparar los modelos necesarios para
         el preprocesado de datos.
 
         X   Matriz bidimiensional con forma (u,v) donde u es el número de tuplas y v el número de variables
@@ -133,7 +133,7 @@ class Preprocessor(object):
         self.desviacion_tipica = np.std(X,  axis = 0)
         self.varianza=np.var(X, axis=0)
         self.IQR = np.subtract(*np.percentile(X, [75, 25], axis = 0))
-        
+
     def transform(self, X_train, X_test, y_train):
         '''
         Recibe los datos de entrada X y opcionalmente los datos de salida esperados y para preprocesar los datos de entrada.
@@ -144,10 +144,10 @@ class Preprocessor(object):
         '''
         #TODO
         X_train, y_train = eliminarRuido(X_train, y_train, self.IQR, self.mediana)
-        
+
         X_train = normalizacionX(X_train,self.mediana,self.IQR,self.media,self.desviacion_tipica)
         X_test = normalizacionX(X_test,self.mediana,self.IQR,self.media,self.desviacion_tipica)
-        
+
         return X_train, X_test, y_train
 
 
@@ -160,7 +160,7 @@ class GradientDescent(object):
         self.thetas = []
         self.mse_ = 99999
         self.mseNew = 99998
-        
+
     def fit(self, X_train, X_test, y_train, y_test):
         '''
         Recibe los datos de entrada X y los datos de salida esperados y para entrenar el modelo de descenso de gradiente.
@@ -174,12 +174,12 @@ class GradientDescent(object):
         self.thetas=[np.random.uniform(-1,1)]*(len(X_train.T))
         while self.mseNew<self.mse_:
             self.fit_step(X_train, y_train)
-            self.mse_ = self.mseNew           
+            self.mse_ = self.mseNew
             self.mseNew=self.mse(X_test, y_test)
-        
+
     def fit_step(self, X, y):
         '''
-        Recibe los datos de entrada X y los datos de salida esperados y para realizar una actualización en batch 
+        Recibe los datos de entrada X y los datos de salida esperados y para realizar una actualización en batch
         de los valores thetas
 
         X   Matriz bidimiensional con forma (u,v) donde u es el número de tuplas y v el número de variables
@@ -188,7 +188,7 @@ class GradientDescent(object):
         '''
         #TODO
         h=calcularh(X_train,self.thetas,self.theta0)
-       
+
         self.theta0, self.thetas =calcularthetas(X_train, y_train, self.alpha, self.theta0, self.thetas ,h)
 
     def predict(self, X):
@@ -203,7 +203,7 @@ class GradientDescent(object):
         y_=np.zeros(len(X_test))
         #Calculamos las prediciones:
         y_=calcularh(X_test,self.thetas,self.theta0)
-            
+
         return y_
 
     def mse(self, X, y):
@@ -224,36 +224,36 @@ class GradientDescent(object):
         errores=(predicion-y_test)
         #Calculo de error cuadratico medio:
         errorcuadraticom=(np.sum(np.square(errores))/(len(X_test)))
-        
+
         return errorcuadraticom
 
 if __name__ == "__main__":
-    
+
     np.random.seed()
     X, y = datasets.load_diabetes(return_X_y=True)
-     #Dividimos los datos en X_train, y_train, X_test y y_train   
+     #Dividimos los datos en X_train, y_train, X_test y y_train
     msk = np.random.rand(len(X)) >= 0.25
     print("Tamaño del dataset",len(y))
     print("Tamaño del train",len(y[msk]))
     print("Tamaño del test",len(y[~msk]))
     X_train = X[msk]
     y_train = y[msk]
-    
+
     X_test = X[~msk]
     y_test = y[~msk]
-    
+
     # Preprocesado de datos (utilizar el modelo de preprocesado creado)
     #TODO
     modelo = Preprocessor()
-    modelo.fit(X_train) 
-    
+    modelo.fit(X_train)
+
     print("Tamaño antes de transformar(tuplas,Variables):",len(X_train),", ",len(X_train.T))
     X_train, X_test, y_train=modelo.transform(X_train, X_test, y_train)
     print("Tamaño despues de transformar(tuplas,Variables):",len(X_train),", ",len(X_train.T))
-    
-   
+
+
     # Creación y entrenamiento del modelo de descenso de gradiente
-    #TODO 
+    #TODO
     modelo = GradientDescent(0.02)
     modelo.fit(X_train, y_train, X_test, y_test)
 
